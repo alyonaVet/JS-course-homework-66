ac7bc8436ab0cf7a83064caea586b90456cb9856
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-07 12:17:02 +0600

    created component MealForm;

---
+++ src/components/MealForm/MealForm.tsx
@@ -0,0 +1,81 @@
+import React, {useState} from 'react';
+import {Meal, MealMutation} from '../../types';
+
+interface MealFormProps {
+  onSubmit: (meal: Meal) => void;
+}
+
+const MealForm: React.FC<MealFormProps> = ({onSubmit}) => {
+  const [mealMutation, setMealMutation] = useState<MealMutation>({
+    receptionTime: '',
+    description: '',
+    calories: '',
+  });
+
+  const changeMeal = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const {name, value} = event.target;
+
+    setMealMutation((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    onSubmit({
+      ...mealMutation,
+      calories: parseFloat(mealMutation.calories),
+    });
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <select
+        className="form-select w-25 mb-3 mt-4"
+        id="receptionTime"
+        name="receptionTime"
+        required
+        onChange={changeMeal}
+        value={mealMutation.receptionTime}
+      >
+        <option value="" disabled>Select Category</option>
+        <option value="breakfast">Breakfast</option>
+        <option value="snack">Snack</option>
+        <option value="lunch">Lunch</option>
+        <option value="dinner">Dinner</option>
+      </select>
+      <div className="form-group mt-4">
+        <textarea
+          name="description"
+          id="description"
+          required
+          className="form-control w-50"
+          placeholder="Add a description"
+          onChange={changeMeal}
+          value={mealMutation.description}
+        />
+      </div>
+
+      <div className="form-group mt-4">
+        <input
+          type="number"
+          name="calories"
+          id="calories"
+          required
+          className="form-control w-25 mb-3 d-inline-block"
+          placeholder="Calories"
+          onChange={changeMeal}
+          value={mealMutation.calories}
+        />
+        <span className="ms-4">kcal</span>
+      </div>
+      <button type="submit" className="btn btn-primary mt-4">
+        Save
+      </button>
+    </form>
+  );
+};
+
+export default MealForm;
