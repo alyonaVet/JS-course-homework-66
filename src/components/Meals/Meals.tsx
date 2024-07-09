714fb96b452b3aa8d3b4d6dbb86cafef167fa5bc
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-09 16:25:54 +0600

    added contentLoading;

--- src/components/Meals/Meals.tsx
+++ src/components/Meals/Meals.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import {Meal} from '../../types';
 import MealItem from './MealItem';
+import Spinner from '../Spinner/Spinner';
 
 interface MealsProps {
   meals: Meal[];
   editMeal: (meal: Meal) => void;
   deleteMeal: (id: string) => void;
+  contentLoading: boolean;
+  deletingMeal: string | null;
 }
-const Meals: React.FC<MealsProps> = ({meals, editMeal, deleteMeal}) => {
+const Meals: React.FC<MealsProps> = ({meals, editMeal, deleteMeal, contentLoading, deletingMeal}) => {
   return (
     <div className="container">
-      {meals.map((meal: Meal) => (
-        <MealItem
-          key={meal.id}
-          meal={meal}
-          onEdit={() => editMeal(meal)}
-          onDelete={() => deleteMeal(meal.id)}
-        />
-      ))}
+      {contentLoading ? (
+        <Spinner />
+      ) : (
+        meals.map((meal: Meal) => (
+          <MealItem
+            key={meal.id}
+            meal={meal}
+            onEdit={() => editMeal(meal)}
+            onDelete={() => deleteMeal(meal.id)}
+            disabled={deletingMeal === meal.id}
+          />
+        ))
+      )}
     </div>
   );
 };
