62e342095c38a79bab7abf837fcbe9baba053cf4
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-09 16:28:26 +0600

    added saving state;

--- src/containers/AddMeal/AddMeal.tsx
+++ src/containers/AddMeal/AddMeal.tsx
@@ -2,19 +2,27 @@ import axiosApi from '../../axiosApi';
 import { useNavigate } from 'react-router-dom';
 import MealForm from '../../components/MealForm/MealForm';
 import {ApiMeal} from '../../types';
+import {useState} from 'react';
 
 const AddMeal = () => {
   const navigate = useNavigate();
+  const [saving, setSaving] = useState(false);
+
 
   const createMeal = async (meal: ApiMeal) => {
-    await axiosApi.post('meals.json', meal);
-    navigate('/');
+    try {
+      setSaving(true)
+      await axiosApi.post('meals.json', meal);
+    } finally {
+      setSaving(false);
+      navigate('/');
+    }
   };
 
   return (
     <div className="container mt-4">
       <h4>Add meal</h4>
-        <MealForm onSubmit={createMeal} />
+        <MealForm onSubmit={createMeal} loading={saving} />
     </div>
   );
 };
