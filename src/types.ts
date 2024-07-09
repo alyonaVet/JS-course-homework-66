c8c8bd3e4e2682c17d911224fe89f32743a9c345
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-09 08:35:24 +0500

    added ApiMeal, ApiMeals;

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,18 @@ export interface MealMutation {
 }
 
 export interface Meal {
+  id: string;
   receptionTime: string;
   description: string;
   calories: number;
+}
+
+export interface ApiMeal {
+  receptionTime: string;
+  description: string;
+  calories: number;
+}
+
+export interface ApiMeals {
+  [id: string]: ApiMeal;
 }
