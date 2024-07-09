94fa1121ec208b1322a69f6b7122e6d25736e293
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-09 16:29:47 +0600

    added routes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-const App = () => {
+import Toolbar from './components/Toolbar/Toolbar';
+import {Route, Routes} from 'react-router-dom';
+import MainPage from './containers/MainPage/MainPage';
+import EditMeal from './containers/EditMeal/EditMeal';
+import AddMeal from './containers/AddMeal/AddMeal';
 
+const App = () => {
   return (
     <>
-        Hello World!
+      <header>
+        <Toolbar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainPage/>} />
+          <Route path="/meals/add-meal" element={<AddMeal/>} />
+          <Route path="/meals/edit-meal/:id" element={<EditMeal/>} />
+          <Route path="*" element={<h3 className="mt-3 mb-5 text-center">Page not found</h3>} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 };
 
 export default App;
