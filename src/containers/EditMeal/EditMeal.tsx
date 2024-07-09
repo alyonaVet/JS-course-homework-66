725b5f950c96a4a3842f7ab9a4f69efd23e81798
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-09 16:28:06 +0600

    added loading, saving states;

--- src/containers/EditMeal/EditMeal.tsx
+++ src/containers/EditMeal/EditMeal.tsx
@@ -3,19 +3,32 @@ import {useCallback, useEffect, useState} from 'react';
 import axiosApi from '../../axiosApi';
 import {ApiMeal} from '../../types';
 import MealForm from '../../components/MealForm/MealForm';
+import Spinner from '../../components/Spinner/Spinner';
 
 const EditDish = () => {
   const {id} = useParams();
   const [meal, setMeal] = useState<ApiMeal | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [saving, setSaving] = useState(false);
+
 
   const fetchOneMeal = useCallback(async () => {
-    const {data: meal} = await axiosApi.get<ApiMeal | null>(`/meals/${id}.json`);
-
-    setMeal(meal);
+    try {
+      setLoading(true);
+      const {data: meal} = await axiosApi.get<ApiMeal | null>(`/meals/${id}.json`);
+      setMeal(meal);
+    } finally {
+      setLoading(false);
+    }
   }, [id]);
 
   const updateMeal = async (meal: ApiMeal) => {
-    await axiosApi.put(`/meals/${id}.json`, meal);
+    try {
+      setSaving(true);
+      await axiosApi.put(`/meals/${id}.json`, meal);
+    } finally {
+      setSaving(false);
+    }
   };
 
   useEffect(() => {
@@ -25,9 +38,11 @@ const EditDish = () => {
   return (
     <div className="container mt-4">
       <h4>Edit meal</h4>
-      <div className="col">
-        {meal && <MealForm onSubmit={updateMeal} existingMeal={meal}/>}
-      </div>
+      {loading ? (
+        <Spinner/>
+      ) : (
+        meal && <MealForm onSubmit={updateMeal} existingMeal={meal} loading={saving}/>
+      )}
     </div>
   );
 };
